56d5fb7c1709e6f911b65bd3df836d34cbb8f497
Lem0naise <zac.i.nolan@gmail.com>
2021-07-28 17:19:16 +0100

    update

---
+++ typewriter.js
@@ -0,0 +1,76 @@
+var profs = [
+    "HTML",
+    "Javascript",
+    "Python",
+    "Bash",
+    "C++",
+    "C#",
+    "SQL",
+]
+
+var time = 0;
+
+document.onload = onEntry()
+
+var empty = true;
+
+async function onEntry() {
+    await waitForMs(200);
+    typeSplash();
+}
+
+async function typeSplash(delay = 100) {
+
+    if (time>=profs.length){
+        time = 0;
+    }
+    let splash = document.getElementById("splash");
+
+    await waitForMs(500)
+    
+    //if is empty, put the next one
+    if (empty){
+        var letters = profs[time];
+        let i = 0;
+    
+        while(i<letters.length) {
+            await waitForMs(delay);
+    
+    
+            splash.innerHTML = splash.innerHTML + letters[i];
+            i++;
+        }
+
+        empty = false;
+        time++;
+    }
+
+    //if is already text there, take it away
+    else if (!empty){
+        await waitForMs(250)
+        let i = 0;
+    
+        while(splash.innerHTML.trim() != "I am familiar with") {
+
+            await waitForMs(delay);
+    
+    
+            splash.innerHTML = splash.innerHTML.substring(0, splash.innerHTML.length-1)
+
+        }
+        empty = true;
+
+    }
+
+
+
+
+    typeSplash();
+    return;
+
+}
+
+
+function waitForMs(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
