306785c6249f3f354e44066c525c2e47b991976c
Zac Nolan <zac.i.nolan@gmail.com>
2022-01-23 17:50:29 +0000

    Removed typsplash

--- typewriter.js
+++ typewriter.js
@@ -1,19 +1,3 @@
-var profs = [
-    "HTML.",
-    "Javascript.",
-    "CSS.",
-    "Python.",
-
-]
-
-var colours = [
-    "orange",
-    "yellow",
-    "lightblue",
-    "deepbluesky",
-]
-
-
 
 //Browser detection
 
@@ -59,7 +43,7 @@ var empty = true;
 
 async function onEntry() {
     await waitForMs(200);
-    typeSplash();
+
 
 }
 
@@ -88,67 +72,6 @@ async function portfolio() {
     }
 }
 
-
-async function typeSplash(delay = 100) {
-
-    if (time>=profs.length){
-        time = 0;
-    }
-
-    let splash = document.getElementById("splash");
-
-
-    await waitForMs(500)
-    
-    //if is empty, put the next one
-    if (empty){
-
-
-
-        splash.style.color = colours[time];
-
- 
-        
-        var letters = profs[time];
-        let i = 0;
-    
-        while(i<letters.length) {
-            await waitForMs(delay);
-    
-    
-            splash.innerHTML = splash.innerHTML + letters[i];
-            
-            i++;
-        }
-
-        empty = false;
-        time++;
-    }
-
-    //if is already text there, take it away
-    else if (!empty){
-        await waitForMs(500)
-        let i = 0;
-
-        while(splash.innerHTML.trim() != "") {
-
-            await waitForMs(delay);
-    
-    
-            splash.innerHTML = splash.innerHTML.substring(0, splash.innerHTML.length-1)
-
-        }
-        empty = true;
-
-    }
-
-
-
-
-    typeSplash();
-    return;
-
-}
 
 
 
