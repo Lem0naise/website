dfc34f6b7c5f690dd8fcce26ab1e4d0b30074f77
Zac Nolan <zac.i.nolan@gmail.com>
2022-01-26 20:05:15 +0000

    Added a transition and swapped welcome and portfol

--- js/canvas-script.js
+++ js/canvas-script.js
@@ -2,61 +2,45 @@ function trapezium(e){
 
     var trapez = document.getElementById("trapezium");
     var buttons = document.getElementsByClassName("menu-button");
+    var main_div = document.getElementsByClassName("menu-text-div")[0];
+    //welcome text (text page 2 initially)
+    var welcome_undertext = document.getElementsByClassName("splash-undertext")[0];
 
-    //text page 2
-    var t_pg2 = document.getElementsByClassName("splash-pg2")[0];
     
-
     //alert(e.deltaY);
     if (e.deltaY>0 ){
+        
         //going down
-        trapez.style.left = "-100vw";
-        alert(trapez.style.left);
-        t_pg2.style.opacity = "0%";
+        trapez.style.marginLeft = "-100vw";
+        main_div.style.left = "12vw";
+
+
+        welcome_undertext.classList.remove("splash-undertext-animation");
+        welcome_undertext.style.left = "5vw";
+        welcome_undertext.style.opacity = 0;
+
 
         for (let i=0; i < buttons.length; i++) {
-
             buttons[i].style.color = "#DCDCDC";
-            
         }
     }
     else if (e.deltaY <0){
 
-
         //going up
-        trapez.style.left = "0";
-        alert(trapez.style.left);
-        t_pg2.style.opacity = "100%";
+        trapez.style.marginLeft = "0";
 
+        welcome_undertext.style.opacity = 100;
+        welcome_undertext.classList.add("splash-undertext-animation");
+
+
+        main_div.style.left = "-50%";
 
         for (let i=0; i < buttons.length; i++) {
-
-            buttons[i].style.color = "#1D1D1D";
-            
+            buttons[i].style.color = "#1D1D1D";  
         }
     }
 }
 
-function page_load(){
 
-    var trapez = document.getElementById("trapezium");
-
-    //text page 2
-    var t_pg2 = document.getElementsByClassName("splash-pg2")[0];
-
-    //going up
-
-}
-
-function last_button_load(){
-    
-    var buttons = document.getElementsByClassName("menu-button");
-
-    for (let i=0; i < buttons.length; i++) {
-
-        buttons[i].style.color = "#1D1D1D";
-        
-    }
-}
 
 window.addEventListener('wheel', trapezium);
