8d58e1d573893a5953ff3e3baeccf54cedde4835
Zac Nolan <zac.i.nolan@gmail.com>
2022-01-26 17:56:34 +0000

    Fixed trapezium and button colour aids

--- js/canvas-script.js
+++ js/canvas-script.js
@@ -3,25 +3,16 @@ function trapezium(e){
     var trapez = document.getElementById("trapezium");
     var buttons = document.getElementsByClassName("menu-button");
 
+    //text page 2
+    var t_pg2 = document.getElementsByClassName("splash-pg2")[0];
+    
 
     //alert(e.deltaY);
     if (e.deltaY>0 ){
-
-
         //going down
-        trapez.style.left = "-300vw";
-
-        for (let i=0; i < buttons.length; i++) {
-
-            buttons[i].style.color = "#1D1D1D";
-            
-        }
-    }
-    else if (e.deltaY <0){
-
-
-        //going up
-        trapez.style.left = "-230vw";
+        trapez.style.left = "-100vw";
+        alert(trapez.style.left);
+        t_pg2.style.opacity = "0%";
 
         for (let i=0; i < buttons.length; i++) {
 
@@ -29,7 +20,43 @@ function trapezium(e){
             
         }
     }
+    else if (e.deltaY <0){
 
+
+        //going up
+        trapez.style.left = "0";
+        alert(trapez.style.left);
+        t_pg2.style.opacity = "100%";
+
+
+        for (let i=0; i < buttons.length; i++) {
+
+            buttons[i].style.color = "#1D1D1D";
+            
+        }
+    }
+}
+
+function page_load(){
+
+    var trapez = document.getElementById("trapezium");
+
+    //text page 2
+    var t_pg2 = document.getElementsByClassName("splash-pg2")[0];
+
+    //going up
+
+}
+
+function last_button_load(){
+    
+    var buttons = document.getElementsByClassName("menu-button");
+
+    for (let i=0; i < buttons.length; i++) {
+
+        buttons[i].style.color = "#1D1D1D";
+        
+    }
 }
 
 window.addEventListener('wheel', trapezium);
