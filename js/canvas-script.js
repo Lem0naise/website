3f8a28d7a1f7c4b9413338ce615e85b10b8b17b1
Zac Nolan <zac.i.nolan@gmail.com>
2022-01-27 19:51:05 +0000

    Made portfolio appear

--- js/canvas-script.js
+++ js/canvas-script.js
@@ -6,6 +6,7 @@ function trapezium(e){
     var trapez = document.getElementById("trapezium");
     var buttons = document.getElementsByClassName("menu-button"); //list of buttons not actual buttons div
     var main_div = document.getElementsByClassName("menu-text-div")[0];
+    var portfolio_div = document.getElementsByClassName("portfolio")[0];
     //welcome text (text page 2 initially)
     var welcome_undertext = document.getElementsByClassName("splash-undertext")[0];
 
@@ -16,12 +17,19 @@ function trapezium(e){
         //going down (intro)
 
         trapez.style.marginLeft = "-100vw";  //moves white trapez
-        main_div.style.left = "12vw"; //intro portfolio
+        main_div.style.left = "12vw"; //intro portfolio title
+        portfolio_div.style.left = "-19vw"; //intro portfolio text
+
+        //I AM CURRENTLY TRYING TO MAKE THE PORTFOLIO DIV COME IN IN THE RIGHT TIME
+
+        portfolio_div.style.transition = "opacity .3s, left .5s .5s";
+        main_div.style.transition = "opacity .3s, left .5s ease"; //makes delay so the text comes in last
 
         
         welcome_undertext.style.transition = "opacity .4s, left 1s"
         welcome_undertext.classList.remove("splash-undertext-animation");
         welcome_undertext.style.left = "5vw";
+
         welcome_undertext.style.opacity = 0;
 
 
@@ -36,6 +44,9 @@ function trapezium(e){
 
         trapez.style.marginLeft = "0"; //puts white trapez back where it belongs
         main_div.style.left = "-100%"; //outro portfolio title
+        portfolio_div.style.left = "-100vw"; //outro portfolio text
+
+        portfolio_div.style.transition = "opacity .3s, left .5s"; //makes delay so the text goes away first
 
 
         welcome_undertext.style.opacity = 100; //fades in scrolltext
