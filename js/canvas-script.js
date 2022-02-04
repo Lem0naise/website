dcb1d7897a05207d5bd3eef5b502e5b1de52c965
Zac Nolan <zac1i1nolan@gmail.com>
2022-02-04 08:43:57 +0000

    Fixed mobile scrolling

--- js/canvas-script.js
+++ js/canvas-script.js
@@ -2,6 +2,8 @@
 //also make the portfolio like actuall stuff appear
 
 var time = false;
+var old_scrolldirection = 0;
+
 
 function trapezium(e){
 
@@ -65,60 +67,76 @@ function trapezium(e){
     }
 
     //if mobile
-    else if (e.deltaY==undefined && !time) {
 
-        time = true;
-
-                
-        //going down (intro)
-        trapez.style.marginLeft = "-100vw";  //moves white trapez
-        main_div.style.left = "12vw"; //intro portfolio title
-        portfolio_div.style.left = "-19vw"; //intro portfolio text
-
-        //I AM CURRENTLY TRYING TO MAKE THE PORTFOLIO DIV COME IN IN THE RIGHT TIME
-
-        portfolio_div.style.transition = "opacity .3s, left .5s .5s";
-        main_div.style.transition = "opacity .3s, left .5s ease"; //makes delay so the text comes in last
-
+    else if (e.deltaY==undefined){ 
         
-        welcome_undertext.style.transition = "opacity .4s, left 1s"
-        welcome_undertext.classList.remove("splash-undertext-animation");
-        welcome_undertext.style.left = "5vw";
 
-        welcome_undertext.style.opacity = 0;
+        var scrolldirection = e.changedTouches[0].screenY;
+
+        if (scrolldirection < old_scrolldirection) {
+
+            //if going down (intro)
+            time = true;
+
+                    
+            //going down (intro)
+            trapez.style.marginLeft = "-100vw";  //moves white trapez
+            main_div.style.left = "12vw"; //intro portfolio title
+            portfolio_div.style.left = "-19vw"; //intro portfolio text
+
+            //I AM CURRENTLY TRYING TO MAKE THE PORTFOLIO DIV COME IN IN THE RIGHT TIME
+
+            portfolio_div.style.transition = "opacity .3s, left .5s .5s";
+            main_div.style.transition = "opacity .3s, left .5s ease"; //makes delay so the text comes in last
+
+            
+            welcome_undertext.style.transition = "opacity .4s, left 1s"
+            welcome_undertext.classList.remove("splash-undertext-animation");
+            welcome_undertext.style.left = "5vw";
+
+            welcome_undertext.style.opacity = 0;
 
 
-        for (let i=0; i < buttons.length; i++) {
-            buttons[i].style.color = "#DCDCDC";
+            for (let i=0; i < buttons.length; i++) {
+                buttons[i].style.color = "#DCDCDC";
+            }
         }
-    }
-    else if (e.deltaY==undefined && time){
-        time = false;
-
-        //going up (outro)
-
-        trapez.style.marginLeft = "0"; //puts white trapez back where it belongs
-        main_div.style.left = "-100%"; //outro portfolio title
-        portfolio_div.style.left = "-100vw"; //outro portfolio text
-
-        portfolio_div.style.transition = "opacity .3s, left .5s"; //makes delay so the text goes away first
 
 
-        welcome_undertext.style.opacity = 100; //fades in scrolltext
-        welcome_undertext.style.transition = "none";
-        welcome_undertext.style.left = "-50vw"; //but moves to left 
-        welcome_undertext.classList.add("splash-undertext-animation"); //restarts animation
-
-
-        for (let i=0; i < buttons.length; i++) {
-            buttons[i].style.color = "#1D1D1D";  
+        //if going back up
+        if (scrolldirection >= old_scrolldirection){
+            time = false;
+    
+            //going up (outro)
+    
+            trapez.style.marginLeft = "0"; //puts white trapez back where it belongs
+            main_div.style.left = "-100%"; //outro portfolio title
+            portfolio_div.style.left = "-100vw"; //outro portfolio text
+    
+            portfolio_div.style.transition = "opacity .3s, left .5s"; //makes delay so the text goes away first
+    
+    
+            welcome_undertext.style.opacity = 100; //fades in scrolltext
+            welcome_undertext.style.transition = "none";
+            welcome_undertext.style.left = "-50vw"; //but moves to left 
+            welcome_undertext.classList.add("splash-undertext-animation"); //restarts animation
+    
+    
+            for (let i=0; i < buttons.length; i++) {
+                buttons[i].style.color = "#1D1D1D";  
+            }
+                    
         }
-                
+
+        old_scrolldirection = scrolldirection;
     }
+
+
 }
 
 function mobile_trapez(e) {
 
+    console.log(e);
     trapezium(e);
 }
 
