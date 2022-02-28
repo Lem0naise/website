e328b8d3d032e72974bb66947cb119b755ba3e06
Zac Nolan <zac1i1nolan@gmail.com>
2022-02-28 16:45:49 +0000

    Added image that appears when you hover over

--- js/script.js
+++ js/script.js
@@ -1,13 +1,35 @@
-var 
+var tings = [
+  '1',
+  '1'
+]
 
-
-
-function slide_out(){
-
-    let img = document.getElementById("img");
-
-    img.style.opacity = 1;
+var to_img = {
+  "Wordle": '#wordle_img',
+  "This website": '#website_img',
 }
 
 
-img.onmouseover() = slide_out()
+$('#website_a').hover(function() {
+
+  $('#website_img').css('opacity', tings[0]);
+
+  if(tings[0]=='1'){
+    tings[0] = '0';
+  }
+  else{
+    tings[0] = '1';
+  }
+})
+
+
+$('#wordle').hover(function() {
+  
+  $('#wordle_img').css('opacity', tings[1]);
+  if(tings[1]=='1'){
+    tings[1] = '0';
+  }
+  else{
+    tings[1] = '1';
+  }
+})
+
