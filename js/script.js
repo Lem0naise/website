5eb465a7fefe35b1d85045eab13861bb11aabd1f
Zac Nolan <zac1i1nolan@gmail.com>
2022-03-21 15:14:25 +0000

    Hamburger menu

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
+let hamburger_int = 0
+
+
+function hamburger() {
+
+  let buttons = document.getElementById("button_margin");
+  let hamburger = document.getElementById("hamburger");
+
+  if (hamburger_int == 0){
+
+    hamburger.style.transform = "rotate(90deg)";
+    buttons.style.opacity = "1";
+    buttons.style.marginLeft = "5vw";
+    hamburger_int = 1;
+  }
+  else{
+
+    hamburger.style.transform = "";
+    buttons.style.opacity = "0";
+    buttons.style.marginLeft = "-20vw";
+    hamburger_int = 0;
+  }
+
+}
+
+
 var tings = [
   '1',
   '1'
 ]
-
 
 function downloadfile(item){
   window.open(item.getAttribute("data-link"));
