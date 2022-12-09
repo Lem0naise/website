d9e4a67adb622df9a7940746d90075fb911ce56e
Zac Nolan <zac1i1nolan@gmail.com>
2022-12-09 20:42:20 +0000

    Colour Palette Generator

--- js/script.js
+++ js/script.js
@@ -1,14 +1,18 @@
-function openLink(e){
-    window.open(items[e.id][2])
-}
+function openLink(e, internal){
+    let target
+    if (internal) {target="_self"}
+    else{target="_blank"}
+    window.open(items[e.id][2], target)
+} 
 function github(){
     window.open("https://github.com/Lem0naise");
 }
 
-var items = [
-    ["Fantasteroids", "An arcade game written in C++. It's on Steam for free!", "https://store.steampowered.com/app/1790870/Fantasteroids/"],
-    ["Wordle ", "A word game playable in your terminal, written in Python.", "https://github.com/Lem0naise/wordle"],
-    ["Student Robotics 2023", "An entry to the 2023 Student Robotics Contest.", "https://github.com/Lem0naise/student-robotics-ham"]
+var items = [ 
+    ["Fantasteroids", "An arcade game written in C++. It's on Steam for free!", "https://store.steampowered.com/app/1790870/Fantasteroids/", false],
+    ["Wordle ", "A word game playable in your terminal, written in Python.", "https://github.com/Lem0naise/wordle", false],
+    ["Student Robotics 2023", "An entry to the 2023 Student Robotics Contest.", "https://github.com/Lem0naise/student-robotics-ham", false],
+    ["Colour Palette Generator", "A simple web-based colour palette generator for graphic designers.", "palette/palette.html", true]
 ]
 
 var port = document.getElementById("portfolio")
@@ -18,7 +22,7 @@ for (let i = 0; i < items.length; i++){
     new_obj.classList.add("item")
     new_obj.style.cursor = "pointer";
     new_obj.setAttribute("id", i)
-    new_obj.setAttribute("onclick", "openLink(this);")
+    new_obj.setAttribute("onclick", `openLink(this, ${items[i][3]});`)
 
     let new_title = document.createElement("p");
     new_title.innerText = items[i][0];
