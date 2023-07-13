cd91f421a9f9fd754575c5e8c448ff7f2ffdb078
Zac Nolan <zac1i1nolan@gmail.com>
2023-07-13 11:45:53 +0100

    Fixed reaction page

--- reaction/reaction.js
+++ reaction/reaction.js
@@ -4,32 +4,36 @@ var screen_d
 var text
 
 function home(){
-	window.open("..", "_self");
+	alert("yoo");
+	window.href = '..';
 }
 
-var turned_white = false;
+
+var state = "stopped";
+// stopped, white
 
 function screen_click(){ // when screen clicked
-	if (turned_white){stopwatch_end();}
-	else{ start();}
+	if (state=="white"){stopwatch_end();}
+	else if (state=='stopped'){ start();}
 }
 
 function start(){
+	state = "timer";
 	screen_d = document.getElementById("screen");
 	text = document.getElementById("text");
 	text.innerText = "Click when the screen turns white..."
 
-	let max = 4;
-	let min = 1;
+	let max = 4000;
+	let min = 1000;
 	let wait = Math.random() * (max - min + 1) + min; // random wait time 
 
 	setTimeout(function(){ // once screen turns white
 		screen_d.style.setProperty("background-color", white); 
 		text.style.setProperty("color", black);
 		text.innerText = "Go!";
-		turned_white = true;
+		state = 'white';
 		stopwatch_start();
-	}, wait*1000)
+	}, wait)
 }
 
 
@@ -40,13 +44,12 @@ var start_d = new Date;
 var end_d = new Date;
 
 function stopwatch_start(){ // when screen turns white
-	start_d = new Date();
+	start_d = Date.now()
 }
 function stopwatch_end(){
-	turned_white = false;
-	end_d = new Date();
-	t_taken = new Date(end_d - start_d).getTime();
-
+	state = 'calculating';
+	end_d = Date.now()
+	t_taken = end_d - start_d;
 	let time_str = "Reaction time of "
 	if (t_taken < 1000){
 		time_str += t_taken + "ms"
@@ -58,5 +61,5 @@ function stopwatch_end(){
 	screen_d.style.setProperty("background-color", black);
 	text.style.setProperty("color", white);
 	text.innerHTML = time_str + ".<br>Click anywhere to try again.";
-
+	state = 'stopped';
 }
