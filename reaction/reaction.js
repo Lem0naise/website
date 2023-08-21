1b25e9d935a085d139e94637f1ee1cccf2ea3d6d
Zac Nolan <zac1i1nolan@gmail.com>
2023-08-21 23:19:47 +0100

    Added too early to reaction game

--- reaction/reaction.js
+++ reaction/reaction.js
@@ -1,7 +1,9 @@
 const black = "#292429"
 const white = "#e8e6e3"
+const red = "#962942"
 var screen_d
 var text
+var timer
 
 function home(){
 	alert("yoo");
@@ -15,24 +17,45 @@ var state = "stopped";
 function screen_click(){ // when screen clicked
 	if (state=="white"){stopwatch_end();}
 	else if (state=='stopped'){ start();}
+	else if (state =='timer'){ early();}
 }
 
 function start(){
+	clearTimeout(timer);
 	state = "timer";
+	text = document.getElementById("text");
+	screen_d = document.getElementById("screen");
+	screen_d.style.setProperty("background-color", black);
+	text.style.setProperty("color", white);
 	screen_d = document.getElementById("screen");
 	text = document.getElementById("text");
-	text.innerText = "Click when the screen turns white..."
+	text.innerText = "Wait..."
 
 	let max = 4000;
 	let min = 1000;
 	let wait = Math.random() * (max - min + 1) + min; // random wait time 
 
-	setTimeout(function(){ // once screen turns white
+	timer = setTimeout(function(){ // once screen turns white
 		screen_d.style.setProperty("background-color", white); 
 		text.style.setProperty("color", black);
 		text.innerText = "Go!";
 		state = 'white';
 		stopwatch_start();
+	}, wait)
+}
+
+function early(){
+	clearTimeout(timer);
+
+	state = 'stopped';
+	text = document.getElementById("text");
+	text.innerText = "Too early!"
+	screen_d = document.getElementById("screen");
+	screen_d.style.setProperty("background-color", red);
+
+	let wait = 1000;
+	timer = setTimeout(function(){ 	
+		start()
 	}, wait)
 }
 
